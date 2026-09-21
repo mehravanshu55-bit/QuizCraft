eee0efb4a274a9a4184ff1ec4f179348ed0953f0
mehravanshu55-bit <mehravanshu55@gmail.com>
2026-09-21 17:05:38 +0530

    Add regex patterns for concept extraction

--- concept_extraction.py
+++ concept_extraction.py
@@ -6,4 +6,10 @@ def extract_concepts(text):
         ("definition", r"^(.+?)\s+is\s+(?:a|an|the)\s+(.+)$"),
         ("definition", r"^(.+?)\s+is\s+(.+)$"),
         ("definition", r"^(.+?)\s+are\s+(.+)$"),
+        ("use", r"^(.+?)\s+is\s+used\s+for\s+(.+)$"),
+        ("use", r"^(.+?)\s+uses\s+(.+)$"),
+        ("support", r"^(.+?)\s+supports\s+(.+)$"),
+        ("provide", r"^(.+?)\s+provides\s+(.+)$"),
+        ("allow", r"^(.+?)\s+allows\s+(.+)$"),
+        ("help", r"^(.+?)\s+helps\s+(.+)$")
     ]
