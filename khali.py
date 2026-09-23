f3ec07b86e3f5f99546f9cd880b9923f3ef4b487
mehravanshu55-bit <mehravanshu55@gmail.com>
2026-09-23 09:50:25 +0530

    Implement score calculation for quiz answers
    Added a function to calculate quiz scores based on user answers.

--- khali.py
+++ khali.py
@@ -1,1 +1,9 @@
+def calculate_score(answers, correct_answers):
+    """Calculate the number of correct quiz answers."""
+    score = 0
 
+    for user_answer, correct_answer in zip(answers, correct_answers):
+        if user_answer == correct_answer:
+            score += 1
+
+    return score
